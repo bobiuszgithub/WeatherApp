85be7c71ae008bbadb59bc0ea31dc33124dfe82b
Cs Bence <bcsaldi18@gmail.com>
2025-04-17 16:56:08 +0200

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Bence Családi on 2025. 04. 17..
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
