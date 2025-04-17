e28552feb1d3a250b521042ac08b9983dc84416e
Cs Bence <bcsaldi18@gmail.com>
2025-04-17 17:26:33 +0200

    weather app source code

---
+++ WeatherApp/WeatherModel.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherModel.swift
+//  WeatherApp
+//
+//  Created by Bence Családi on 2025. 04. 17..
+//
+
+import SwiftUI
+
+struct WeatherModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherModel()
+}
---
+++ WeatherApp/WeatherViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherViewModel.swift
+//  WeatherApp
+//
+//  Created by Bence Családi on 2025. 04. 17..
+//
+
+import SwiftUI
+
+struct WeatherViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherViewModel()
+}
